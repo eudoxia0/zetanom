ad63d9585a072a17ecbc8693f4b9254bd71061cc
Fernando Borretti <fernando@borretti.me>
2025-11-08 12:38:59 +1100

    add hello world route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+
+const PORT: u16 = 12001;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{PORT}")).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
