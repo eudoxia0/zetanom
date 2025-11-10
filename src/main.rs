5a664ced6b4143f2d21ee88bbf51aa4c7a361f43
Fernando Borretti <fernando@borretti.me>
2025-11-10 22:03:00 +1100

    print

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ const PORT: u16 = 12001;
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{PORT}")).await.unwrap();
+    let bind = format!("0.0.0.0:{PORT}");
+    println!("Started server on {bind}.");
+    let listener = tokio::net::TcpListener::bind(bind).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
