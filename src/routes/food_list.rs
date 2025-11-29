f3e9adb820f8a3e823c915d9f215593a0fb842da
Fernando Borretti <fernando@borretti.me>
2025-11-29 13:43:44 +1100

    style

--- src/routes/food_list.rs
+++ src/routes/food_list.rs
@@ -49,51 +49,35 @@ async fn handler(State(state): State<ServerState>) -> Fallible<(StatusCode, Html
             }
         }
     } else {
-        let columns = vec![
-            TableColumn {
-                header: "Name".to_string(),
-                numeric: false,
-            },
-            TableColumn {
-                header: "Brand".to_string(),
-                numeric: false,
-            },
-        ];
-
-        let rows = html! {
-            @for food in &foods {
-                tr {
-                    td {
-                        a href=(FoodViewHandler::url(food.food_id)) {
-                            (food.name)
-                        }
-                    }
-                    td {
-                        @if food.brand.is_empty() {
-                            "—"
-                        } @else {
-                            (food.brand)
-                        }
-                    }
-                }
-            }
-        };
-
         html! {
             table {
                 thead {
                     tr {
-                        @for col in columns {
-                            @if col.numeric {
-                                th.numeric { (col.header) }
-                            } @else {
-                                th { (col.header) }
-                            }
+                        th {
+                            "Name"
+                        }
+                        th {
+                            "Brand"
                         }
                     }
                 }
                 tbody {
-                    (rows)
+                    @for food in &foods {
+                        tr {
+                            td {
+                                a href=(FoodViewHandler::url(food.food_id)) {
+                                    (food.name)
+                                }
+                            }
+                            td {
+                                @if food.brand.is_empty() {
+                                    "—"
+                                } @else {
+                                    (food.brand)
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
